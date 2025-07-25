c09063af440cd19e0288d46f004337aec2f96351
Sanju <79065769+sanjukhetavath@users.noreply.github.com>
2025-07-25 19:40:52 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,8 @@
+# Resource Group Module
+module "resoruce_group" {
+  source = "https://github.com/sanjukhetavath/module_resoruce_group.git/modules/resource_group"
 
+  name     = var.resoruce_group
+  location = var.location
+  tags     = var.tags
+}
