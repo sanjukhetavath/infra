9c7f176e77e9982cdf1c5f04d8a53d853c784c92
Sanju khetavath <sanju@Sanjus-MacBook-Pro.local>
2025-07-27 15:47:11 +1000

    update

--- variables.tf
+++ variables.tf
@@ -1,1 +1,26 @@
 
+variable "client_id" {
+  type        = string
+  description = "Azure Client id"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Azure client Secret "
+}
+
+variable "tenant_id" {
+  type        = string
+  description = " Azure tenant_id"
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "Azure subscription id"
+}
+
+variable "location" {
+  type        = string
+  description = "Resource group Location"
+  default     = "Australia East"
+}
